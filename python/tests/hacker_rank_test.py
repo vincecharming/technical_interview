7800ed888c25e99fdbd902547d8255ea05ad7344
Vincent Charming <vince@charmingmagic.com>
2019-08-19 14:02:47 -0400

    Update hacker_rank_test.py

--- python/tests/hacker_rank_test.py
+++ python/tests/hacker_rank_test.py
@@ -1,5 +1,6 @@
 """
-Tests for HackerRank Sample Problems for SWE I Interview
+Vincent Charming (c) 2019
+Tests for HackerRank Sample Problems
 Note: Normally larger data sets would be imported from a YAML config file to test time complexity
 """
 
